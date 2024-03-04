2d1717921b9b65d5ea7cf8ba31c71521f4b35b2d
Kennedy Umeigbo <kennyumeigbo@yahoo.com>
2024-03-04 16:40:50 +0100

    added test objects

---
+++ script.py
@@ -0,0 +1,11 @@
+# list of our destinations
+destintions = [
+    "Paris, France",
+    "Shanghai, China",
+    "Los Angeles, USA",
+    "São Paulo, Brazil",
+    "Cairo, Egypt"
+]
+
+# test traveler for testing functionality
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
