8556111ecd8f7229ec5b9480dc1425c6b2a9a575
Kennedy Umeigbo <kennyumeigbo@yahoo.com>
2024-03-05 02:08:00 +0100

    Added function to generate message for traveler and present attractions they might be interested in.

--- script.py
+++ script.py
@@ -42,7 +42,7 @@ def add_attraction(destination, attraction):
 
 # testing the add_attraction function
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
-print(attractions)
+# print(attractions)
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
@@ -53,7 +53,7 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print(attractions)
+# print(attractions)
 
 # function to help tourists find the most interesting places in a new city, that matches their interests with possible locations in the city
 def find_attractions(destination, interests):
@@ -70,3 +70,17 @@ def find_attractions(destination, interests):
 # testing out the find_attractions function
 la_arts = find_attractions("Los Angeles, USA", ["art"])
 print(la_arts)
+
+# function to connect people with the attractions they are interested in
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+    interests_string = f"Hi {traveler[0]}" + f", we think you'll like these places around {traveler_destination}: "
+    for possible_attraction in traveler_attractions:
+        interests_string = interests_string + possible_attraction + ","
+    return interests_string
+
+# testing out the get_attractions_for_traveler function
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
